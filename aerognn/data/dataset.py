969a68587be5ea664d06d39008e563122fb916aa
Dhanya Ganesh <dhanyag1809@gmail.com>
2026-03-22 14:45:31 -0700

    Added dataset class

---
+++ aerognn/data/dataset.py
@@ -0,0 +1,17 @@
+import torch
+from torch_geometric.data import Dataset
+import os
+ 
+class BuildingDataset(Dataset):
+    def __init__(self, processed_dir='data/processed/'):
+        super().__init__()
+        self.file_list = sorted([
+            os.path.join(processed_dir, f)
+            for f in os.listdir(processed_dir) if f.endswith('.pt')
+        ])
+ 
+    def len(self):
+        return len(self.file_list)
+ 
+    def get(self, idx):
+        return torch.load(self.file_list[idx], weights_only=False)
