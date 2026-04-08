1dd291b40ebe2b765ff660809b389b7de3acc363
Dhanya Ganesh <dhanyag1809@gmail.com>
2026-04-08 10:20:06 -0700

    Fixed bugs

--- scripts/cross_validate.py
+++ scripts/cross_validate.py
@@ -2,7 +2,7 @@ from aerognn.data.dataset import BuildingDataset
 from aerognn.training.trainer import cross_validation
 def cv():
     dataset = BuildingDataset()
-    mae, r2 = cross_validation(dataset, 150)
+    mae, r2 = cross_validation(dataset, 300)
     print(f'MAE: {mae}, R^2: {r2}')
 
 if __name__ == "__main__":
