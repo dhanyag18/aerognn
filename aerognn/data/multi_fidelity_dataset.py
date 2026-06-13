00ed6551942251792207eea96677909a626ea97e
Dhanya Ganesh <dhanyag1809@gmail.com>
2026-06-12 21:38:45 -0700

    Small updates

--- aerognn/data/multi_fidelity_dataset.py
+++ aerognn/data/multi_fidelity_dataset.py
@@ -1,24 +1,34 @@
-import torch 
+import torch
 from aerognn.data.dataset import BuildingDataset
+
+
 class MultiFidelityDataset:
-   
+
     def __init__(self, coarse_dir='data/coarse/',
                  fine_dir='data/processed/'):
-        self.coarse = BuildingDataset(coarse_dir)
-        self.fine = BuildingDataset(fine_dir)
-    
+        self.coarse_dir = coarse_dir
+        self.fine_dir = fine_dir
+
+    @property
+    def coarse(self):
+        return BuildingDataset(self.coarse_dir)
+
+    @property
+    def fine(self):
+        return BuildingDataset(self.fine_dir)
+
     def get_paired(self):
         coarse_ids = {d.id for d in self.coarse}
         fine_ids = {d.id for d in self.fine}
         paired_ids = coarse_ids & fine_ids
         return (
             [d for d in self.coarse if d.id in paired_ids],
-            [d for d in self.fine if d.id in paired_ids]
+            [d for d in self.fine   if d.id in paired_ids]
         )
-    
+
     def add_simulation(self, graph, resolution):
         if resolution == 'coarse':
-            path = f'data/coarse/building_{graph.id}.pt'
+            path = f'data/coarse/{graph.id}.pt'
         else:
-            path = f'data/processed/building_{graph.id}.pt'
+            path = f'data/processed/{graph.id}.pt'
         torch.save(graph, path)
